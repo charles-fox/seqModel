76c00c8b875b463e4a5958cffa7ff66df9ed5022
fantastic-robot <tsfc@leeds.ac.uk>
2018-05-08 19:34:02 +0100

    plot mutual information over time for each interaction given some observed features

--- timeFilter.py
+++ timeFilter.py
@@ -8,6 +8,7 @@ Created on Sun May  6 16:30:03 2018
 from goTime import  *
 from winnerMatrix import *
 from entropy import *
+import matplotlib.pyplot as plt
 
 
 # to deal with zeros for log funcion
@@ -120,26 +121,87 @@ def descInfoGain(pDesc):
 		I_2Ddesc.append(I_2D(ps))
 		
 	return I_2Ddesc
-		
-def initPlot(I_2Ddesc, lDesc, descriptorss):
+	
+# compute cumulative probabilities for each descriptor	
+def initProb(I_2Ddesc, lDesc, descriptorss):
 	print("\n init \n")
 	
 	initprob = []
 	for descriptor in descriptorss:
-		p = 1
 		for i in range(0, len(lDesc)):
 			if any( lDesc[i] in s for s in descriptor): # single interacting car
-				if(i == 10):
+				if(i == 0):
+					p = I_2Ddesc[i]				
+				elif(i == 10):
 					if any( "Distraction_None" in s for s in descriptor): 
 						continue
 					else:
-						p *= I_2Ddesc[i]
+						p *= I_2Ddesc[i]	
 				else:
 					p *= I_2Ddesc[i]
 				
 		initprob.append(p)
 		print(p)
-				
+	return initprob
+	
+
+# compute cumulative probabilities for each sequence
+def seqProb(I_2Dseq, seqs):
+	
+	print("\n seqProb \n")	
+	
+	pseq = []
+		
+	for i in range(0, 203):
+		print("i :" + str(i))
+		p = []
+		for j in range(0, len(seqs[i])):
+			print("j: " + str(j))
+			if(j == 0):
+				p.append(I_2Dseq[seqs[i][j]])
+				print(I_2Dseq[seqs[i][j]])
+			else:
+				print(I_2Dseq[seqs[i][j-1]])
+				print(I_2Dseq[seqs[i][j]] )
+				p.append(I_2Dseq[seqs[i][j-1]] * I_2Dseq[seqs[i][j]])
+			
+		print(p)	
+		pseq.append(p)
+	
+	#print(pseq)
+	return pseq
+	
+# compute X (time) and Y(prob) for plot 	
+def prepapreXY(descpb, seqpb, timepb):
+	
+	print("\n prepareXY \n")	
+	
+	x = 	[]
+	x.append(0)
+	
+	y = []
+	y.append(descpb)
+	
+	for i in range(0, len(seqpb)):
+		x.append(timepb / len(seqpb)) 
+		
+	for i in range(0 , len(seqpb)):
+		y.append((y[i] * seqpb[i]))
+
+	x = np.cumsum(x)	
+	
+	print("X : " + str(x))
+	print("Y : " + str(y))
+	return x, y
+	
+def plotXY(X, Y):
+	
+	plt.plot(X, np.log(Y))
+	plt.title("Mutual information I(W|Data(i:t)) in function of the time")
+	plt.xlabel("Time (s)")
+	plt.ylabel("I(W|Data(i:t))")
+	plt.show()
+	
 	
 if __name__=="__main__":
 	dct_noneEvents = loadNoneEvents()
@@ -163,4 +225,11 @@ if __name__=="__main__":
 	I_2Dseq = seqInfoGain(pSeq)
 	I_2Ddesc = descInfoGain(pDesc)
 	
-	initPlot(I_2Ddesc, lDesc, descriptorss)
+	initprob = initProb(I_2Ddesc, lDesc, descriptorss)
+	
+	initpseq = seqProb(I_2Dseq, seqs)
+	
+	plt.figure()
+	for i in range(0, 203):
+		X, Y = prepapreXY(initprob[i], initpseq[i], tDelta[i])
+		plotXY(X, Y)
